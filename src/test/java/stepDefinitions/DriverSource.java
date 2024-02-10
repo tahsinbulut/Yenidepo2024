236daae8b17f018991ad054e7afa29a86d6c8660
tahsin <tahsinbulut48@gmail.com>
2024-02-10 21:09:54 +0100

    driversource degisim 240210

--- src/test/java/stepDefinitions/DriverSource.java
+++ src/test/java/stepDefinitions/DriverSource.java
@@ -2,16 +2,23 @@ package stepDefinitions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public enum DriverSource {
     //only one instance
     INSTANCE;
+    ChromeOptions options = new ChromeOptions();
     private WebDriver driver;
 
     public WebDriver getDriver() {
         if(driver == null) {
             driver = new ChromeDriver();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+            //options.addArguments("headless");
         }
         return driver;
+        //only one instance
+
     }
 }

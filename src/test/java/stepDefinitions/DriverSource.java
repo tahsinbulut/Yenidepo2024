c18790fe1dcf7c3f1cf6f5fc594fc5a7937ed4ec
tahsin <tahsinbulut48@gmail.com>
2024-02-16 20:44:29 +0100

    guncelleme

--- src/test/java/stepDefinitions/DriverSource.java
+++ src/test/java/stepDefinitions/DriverSource.java
@@ -5,20 +5,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public enum DriverSource {
+
     //only one instance
     INSTANCE;
     ChromeOptions options = new ChromeOptions();
     private WebDriver driver;
 
-    public WebDriver getDriver() {
-        if(driver == null) {
-            driver = new ChromeDriver();
-            options.addArguments("--headless");
+    //    public WebDriver getDriver(){
+//        if (driver == null){
+//            driver = new ChromeDriver();
+//            //options.addArguments("--headless");// addArguments kullandık.
+//            //driver = new ChromeDriver(options);
+//            options.addArguments("headless");
+//            driver = new ChromeDriver(options);
+//        }
+//        return driver;
+//    }
+    public WebDriver getDriver(){
+        if (driver == null){
+            options.addArguments("--headless"); // Headless argümanı eklendi
             driver = new ChromeDriver(options);
-            //options.addArguments("headless");
         }
         return driver;
-        //only one instance
-
     }
 }
